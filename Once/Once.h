237c1cfb54d5414dbd401cc7729362a027272039
sgr-ksmt <melodydance.k.s@gmail.com>
2016-09-25 00:45:03 +0900

    setup project

---
+++ Once/Once.h
@@ -0,0 +1,19 @@
+//
+//  Once.h
+//  Once
+//
+//  Created by Suguru Kishimoto on 9/25/16.
+//  Copyright © 2016 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Once.
+FOUNDATION_EXPORT double OnceVersionNumber;
+
+//! Project version string for Once.
+FOUNDATION_EXPORT const unsigned char OnceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Once/PublicHeader.h>
+
+
